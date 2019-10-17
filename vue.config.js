e2206f0e8e6ed364dbfe37ea4a8cedbe1410c1b2
berry_cooper <berry_cooepr@163.com>
2019-10-17 11:04:27 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   lintOnSave: false,
   configureWebpack: config => {
+    config.plugins.push(
+      new CopyWebpackPlugin([
+        {
+          from: './static', // 新增可以被index.html访问的静态文件目录,支持多个
+          to: 'static',
+          ignore: ['.*']
+        }
+      ])
+    )
     if (process.env.NODE_ENV === 'production') {
       // 为生产环境修改配置...
       console.log('启动生产环境')
